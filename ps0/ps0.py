01a82e7acf235c2325c2468d9cb16c27dbebff37
MohamedSa3eed <88633195+MohamedSa3eed@users.noreply.github.com>
2021-08-17 16:35:52 +0200

    Add files via upload

---
+++ ps0/ps0.py
@@ -0,0 +1,7 @@
+import math
+
+first_number = int(input("Enter number X: "))
+second_number = int(input("Enter number Y: "))
+
+print("x ** Y = ",first_number**second_number)
+print(F"log(x) = {math.log(first_number, 2)}")
